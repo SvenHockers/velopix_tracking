d17b60394fde41de94b8688c5d3c9c421eb48182
Hockers, Sven (Stud. DACS) <s.hockers@student.maastrichtuniversity.nl>
2025-02-21 16:29:24 +0100

    Added searchByTriplet Algo

--- rust_algorithms/src/lib.rs
+++ rust_algorithms/src/lib.rs
@@ -6,13 +6,14 @@ mod event_model;
 use crate::event_model::event::Event;
 use crate::algorithms::track_following::TrackFollowing;
 use crate::algorithms::graph_dfs::GraphDFS;
-// use crate::algorithms::search_by_triplet_trie;
+use crate::algorithms::search_by_triplet_trie::SearchByTripletTrie;
 
 #[pymodule]
 fn velopix_tracking(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_class::<Event>()?;
     m.add_class::<TrackFollowing>()?;
     m.add_class::<GraphDFS>()?;
+    m.add_class::<SearchByTripletTrie>()?;
     Ok(())
 }
 
