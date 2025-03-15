143aaf22eca932af1849e88134e2e86b6c07a04c
Hockers, Sven (Stud. DACS) <svenhockers1@gmail.com>
2025-03-15 20:01:48 +0100

    refactor to improve understandability of parent class

--- py_modules/velopix_wrapper/parameter_optimisers.py
+++ py_modules/velopix_wrapper/parameter_optimisers.py
@@ -30,7 +30,7 @@ class optimiserBase(ABC):
         This method uses the template method pattern: it calls _generate_next_pMap (implemented by the child)
         to obtain a candidate parameter map, then validates it using self.algorithm's schema.
         """
-        pMap = self._generate_next_pMap()
+        pMap = self.next()
         if self._algorithm is None:
             raise ValueError("Algorithm not set. Please call start() to initialise the algorithm.")
         if self.validate_config(pMap, self._algorithm.value):
@@ -42,20 +42,20 @@ class optimiserBase(ABC):
     
     def start(self, algorithm: ReconstructionAlgorithms) -> Dict[str, Any]:
         self._algorithm = algorithm # this is required for the pMap validation
-        pMap = self._start()
+        pMap = self.init()
         if self.validate_config(pMap, self._algorithm.value):
             return pMap
         raise ValueError("Parameter map validation failed.")
 
     @abstractmethod
-    def _start(self) -> Dict[str, Any]:
+    def init(self) -> Dict[str, Any]:
         """ 
         This is the actual logic for the start call
         """
         pass
 
     @abstractmethod
-    def _generate_next_pMap(self) -> Dict[str, Any]:
+    def next(self) -> Dict[str, Any]:
         """
         Abstract method that should generate the next parameter map.
         Child classes must implement this method. The output of this method will be automatically
@@ -66,7 +66,7 @@ class optimiserBase(ABC):
         pass
 
     @abstractmethod
-    def _convert_results_to_score(self) -> Any:
+    def score(self) -> Any:
         """ 
         Since the output of the velopix track returns a JSON array with various values (eg. ghost rates, clones, etc.)
         some method to convert these stats into a score that can be used by whatever model should be made
