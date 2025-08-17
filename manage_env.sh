72def47c48c925bd2feafe93b6ebc540dc34cfa5
SvenHockers <90685770+SvenHockers@users.noreply.github.com>
2025-08-17 17:28:49 +0200

    rm maturin build (since we rm the Rust code from this repo)

--- manage_env.sh
+++ manage_env.sh
@@ -25,24 +25,6 @@ if [ "$action" == "install" ]; then
         rm -rf "$venv_dir"
     fi
 
-    # check if Rust is installed by looking for rustc
-    if ! command -v rustc &> /dev/null; then
-        echo "Rust is not installed. Installing Rust..."
-        if [ "$os" == "Darwin" ]; then
-            brew install rust
-        else
-            curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-            if [ -f "$HOME/.cargo/env" ]; then
-                echo "Sourcing Rust environment..."
-                source "$HOME/.cargo/env"
-            else
-                echo "Warning: $HOME/.cargo/env not found. Please check your rustup installation."
-            fi
-        fi
-    else
-        echo "Rust is already installed. Skipping Rust installation."
-    fi
-
     echo "Creating a new virtual environment..."
     $python_cmd -m venv "$venv_dir"
 
@@ -50,17 +32,11 @@ if [ "$action" == "install" ]; then
     # shellcheck disable=SC1091
     source "$activate_script"
 
-    export PATH="$HOME/.cargo/bin:$PATH"
-
     echo "Upgrading pip..."
     pip install --upgrade pip
 
     echo "Installing dependencies from requirements.txt..."
     pip install -r requirements.txt
-
-    cd VeloPix
-    maturin develop --release
-    cd ..
 
 elif [ "$action" == "update" ]; then
     if [ ! -d "$venv_dir" ]; then
@@ -79,10 +55,6 @@ elif [ "$action" == "update" ]; then
 
     echo "Updating dependencies from requirements.txt..."
     pip install --upgrade -r requirements.txt
-
-    cd VeloPix
-    maturin develop --release
-    cd ..
 
 else
     echo "Invalid argument: $action"
